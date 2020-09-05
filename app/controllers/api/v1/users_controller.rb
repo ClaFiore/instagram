2c36b0a52127b6a4fa124830f3b48a7f9f0ed7a2
ClaFiore <claborghini@gmail.com>
2020-09-04 22:53:10 -0400

    User controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::UsersController < ApplicationController
+    before_action :find_user, only: [:show, :update, :destroy]
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        render json: @user
+    end
+
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
+
+    def update
+        @user.update(user_params)
+        render json: @user
+    end
+
+    def destroy
+        @user.destroy
+        render json: 'User destroyed'
+    end
+
+private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :bio, :password, :phone, :gender, :birthday)
+    end
+end
